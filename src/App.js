78783a7ccad84069bce50b4b4a2467864deb8a8f
Pahuja <vanpahuja@deloitte.com>
2025-01-08 12:15:02 +0530

    CSS and total in cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from './Components/Checkout/Checkout'; // Adjust the path as nec
 
 import Register from "./Components/Register";
 import Dashboard from './Components/Dashboard';
+import './css/cart.css'
 import Cart from './Components/cart';
 function App() {
   return (
