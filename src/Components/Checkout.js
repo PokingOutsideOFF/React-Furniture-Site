4af84107ad6f32e95ed4d1e96f28c2d3dc5b3972
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2025-01-09 00:23:20 +0530

    Final

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import "../css/checkout.css";
 import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../Context/UserContext";
